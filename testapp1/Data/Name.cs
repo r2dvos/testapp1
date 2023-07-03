eacdfe7531f2da7f924fdf9db5a2631f1a51efc5
Vos <voswesseling@gmail.com>
2023-07-03 14:54:19 +0200

    Initial commit

---
+++ testapp1/Data/Name.cs
@@ -0,0 +1,8 @@
+﻿namespace testapp1.Data
+{
+    public class Name
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ testapp1/Data/TestDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace testapp1.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Name> Names { get; set; } = default!;
+
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Name>().ToTable("Names");
+            modelBuilder.Entity<Name>().HasIndex(x => x.FullName);
+            modelBuilder.Entity<Name>().Property(x => x.FullName).UseCollation("nocase");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.LogTo(Console.WriteLine, LogLevel.Warning)
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging(true);
+        }
+    }
+}
